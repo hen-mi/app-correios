a5ad73c0b4d06c9760a9444880bb33c84289f3f8
Tiago Gurdiano <tiagofran2013@hotmail.com>
2022-11-29 01:23:15 -0300

    Add Obj Pacote and ListView

--- lib/telas/pacotes.dart
+++ lib/telas/pacotes.dart
@@ -1,3 +1,4 @@
+import 'package:correios/repositories/pacote_repository.dart';
 import 'package:flutter/material.dart';
 
 class Pacotes extends StatefulWidget {
@@ -10,11 +11,21 @@ class Pacotes extends StatefulWidget {
 class _PacotesState extends State<Pacotes> {
   @override
   Widget build(BuildContext context) {
+    final tabela = PacoteRepository.tabela;
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue[400],
-        body: Center(
-          child: Text("Pacotess"),
+        body: ListView.separated(
+          itemBuilder: (BuildContext context, int moeda){
+            return ListTile(
+              leading: Image.asset(tabela[moeda].icone),
+              title: Text("Pacote: ${tabela[moeda].codigoRast}"),
+              trailing: Text("${tabela[moeda].peso.toString()}Kg"),
+            );
+          },
+          padding: EdgeInsets.all(16),
+          separatorBuilder: (_, ___) => Divider(),
+          itemCount: tabela.length,
         ),
       ),
     );
